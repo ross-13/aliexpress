a9e662c1d7928b4a271d2429e538d8148c289f6c
ross <rostishka1@gmail.com>
2023-07-23 13:31:35 +0300

    feat: add animation to MenuOverlay in app.vue file

--- app.vue
+++ app.vue
@@ -1,5 +1,38 @@
+<script setup lang="ts">
+import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
+import { useUserStore } from '~/stores/user'
+
+const breakpoints = useBreakpoints(breakpointsTailwind)
+
+const largerThanSm = breakpoints.greater('sm') // only larger than sm
+
+const userStore = useUserStore()
+
+const route = useRoute()
+
+onMounted(() => {
+  userStore.isLoading = true
+})
+
+watch(() => largerThanSm.value, () => {
+  if (largerThanSm)
+    userStore.isMenuOverlay = false
+})
+
+watch(() => route.fullPath, () => {
+  userStore.isLoading = true
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
+
+  <MenuOverlay
+    :class="[
+      { 'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isMenuOverlay },
+      { 'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isMenuOverlay },
+    ]"
+  />
 </template>
