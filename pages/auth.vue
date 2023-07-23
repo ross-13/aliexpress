b1251a7c3af0e7435a64e419b91073f10d8ef029
ross <rostishka1@gmail.com>
2023-07-23 12:09:50 +0300

    feat: add basic auth page.

---
+++ pages/auth.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+async function login(prov: 'google' | 'github') {
+  // login by provider
+}
+watchEffect(() => {
+  // todo:
+})
+definePageMeta({ layout: 'main' })
+</script>
+
+<template>
+  <div id="AuthPage" class="w-full h-[100vh] bg-white">
+    <div class="w-full flex items-center justify-center p-5 border-b border-b-gray-300">
+      <NuxtLink to="/" class="min-w-[170px]">
+        <img width="170" src="/AliExpress-logo.png">
+      </NuxtLink>
+    </div>
+
+    <div class="max-w-[400px] mx-auto px-2">
+      <div class="text-center my-6">
+        Login / Register
+      </div>
+
+      <button
+        class="flex items-center justify-center gap-3 p-1.5 w-full border hover:bg-gray-100 rounded-full text-lg font-semibold"
+        @click="login('google')"
+      >
+        <img class="w-full max-w-[30px]" src="/google-logo.png">
+        <div>Google</div>
+      </button>
+
+      <button
+        class="mt-4 flex items-center justify-center gap-3 p-1.5 w-full border hover:bg-gray-100 rounded-full text-lg font-semibold"
+        @click="login('github')"
+      >
+        <img class="w-full max-w-[30px]" src="/github-logo.png">
+        <div>Github</div>
+      </button>
+    </div>
+  </div>
+</template>
